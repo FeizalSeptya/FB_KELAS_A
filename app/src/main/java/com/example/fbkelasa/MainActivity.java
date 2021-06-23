fd8324e6f5c11627ba78dbcd2d73041984f67021
FeizalSeptya <izal1309@gmail.com>
2021-06-23 21:39:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fbkelasa/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.fbkelasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.fbkelasa.R;
+import com.example.fbkelasa.database.LihatTeman;
+import com.example.fbkelasa.database.TambahTeman;
+
+public class MainActivity extends AppCompatActivity {
+    Button tambahbtn,lihatbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tambahbtn = findViewById(R.id.btnInsert);
+        lihatbtn = findViewById(R.id.btnSelect);
+
+        tambahbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(MainActivity.this, TambahTeman.class);
+                startActivity(intent);
+            }
+        });
+
+        lihatbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(MainActivity.this, LihatTeman.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
